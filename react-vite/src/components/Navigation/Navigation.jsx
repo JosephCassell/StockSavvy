f90ad0d6a6a61d69a3139b27aff492ba63f9ca41
Joseph <yosefcassell@gmail.com>
2024-03-12 01:20:03 -0700

    landingpage

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,48 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
+import logo from '../../../../Photos/Logo2.png';
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation({ user }) {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+    <nav className="navbar">
+      <NavLink to="/" className="logo">
+        <img src={logo} alt="Stock Savvy Logo" />
+      </NavLink>
+      <ul className="nav-links">
+        <li>
+          <NavLink to="/what-we-offer" className="What">WHAT WE OFFER</NavLink>
+        </li>
+        <li>
+          <NavLink to="/gold" className="Gold">GOLD</NavLink>
+        </li>
+        <li>
+          <NavLink to="/learn" className="Learn">LEARN</NavLink>
+        </li>
+        <li>
+          <NavLink to="/snacks" className="Snacks">SNACKS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/support" className="Support">SUPPORT</NavLink>
+        </li>
+      </ul>
+      {!user && (
+        <div className="auth-buttons">
+          <OpenModalMenuItem
+            itemText="Log In"
+            modalComponent={<LoginFormModal />}
+          />
+          <OpenModalMenuItem
+            itemText="Sign Up"
+            modalComponent={<SignupFormModal />}
+          />
+        </div>
+      )}
+      {user && <ProfileButton />}
+    </nav>
   );
 }
 
