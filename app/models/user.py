0f8e12cbe026b9bbb4708172e54fb4991fe63d6b
Landon <landon.hurst@outlook.com>
2024-03-12 19:50:00 -0400

    made database models and relationships

--- app/models/user.py
+++ app/models/user.py
@@ -16,6 +16,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    watchlists = db.relationship('Watchlist', back_populates='user')
+    portfolios = db.relationship('Portfolio', back_populates='user')
+
     @property
     def password(self):
         return self.hashed_password
