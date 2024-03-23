46b35baa8a6e61c46a7c085f4b8fb03b66e464b5
Joseph <yosefcassell@gmail.com>
2024-03-22 18:39:20 -0700

    stockdetails graph done

--- react-vite/src/redux/session.js
+++ react-vite/src/redux/session.js
@@ -31,6 +31,7 @@ export const thunkLogin = (credentials) => async dispatch => {
 
   if (response.ok) {
     const data = await response.json();
+    console.log('Login data received:', data);
     dispatch(setUser(data));
   } else if (response.status < 500) {
     const errorMessages = await response.json();
@@ -66,6 +67,7 @@ export const thunkLogout = () => async (dispatch) => {
 const initialState = { user: null };
 
 function sessionReducer(state = initialState, action) {
+  console.log('Reducer action received:', action);
   switch (action.type) {
     case SET_USER:
       return { ...state, user: action.payload };
